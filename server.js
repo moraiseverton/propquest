db9fec8f2cf331234dc663af570cb90477969c52
Everton Morais <esdmorais@gmail.com>
2020-05-06 23:48:55 -0500

    Adding node express + basic server configuration + gitignore file.

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('prorequest!'))
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
